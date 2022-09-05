313a33bf8742139d0f141216a4ac53eb526e7053
Anatoly Stadnichuk <88707999+anatolycider@users.noreply.github.com>
2022-09-05 18:59:14 +0100

    Update diff.js

--- diff.js
+++ diff.js
@@ -9,8 +9,8 @@ router.get('/greeting', (req, res) => {
 
 //template handle escaping 
 router.get('/greet-template', (req, res) => {
-    name = req.query.name
-    res.render('index', { user_name: name });
+    //name = req.query.name
+    //res.render('index', { user_name: name });
 })
 
 module.exports = router
